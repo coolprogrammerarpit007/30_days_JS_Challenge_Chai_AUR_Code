76f91a8d177b01fc6ede9aee150cd06daffc75be
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-14 13:57:29 +0530

    completed day-2

---
+++ day_2/index.js
@@ -0,0 +1,22 @@
+`use strict`;
+console.log(25+25);
+console.log(25-12);
+console.log(25*12);
+console.log(25/25);
+console.log(25%25);
+
+let num = 25;
+console.log(num += 25);
+console.log(num -= 25);
+console.log(5>3);
+console.log(5<3);
+console.log(5<=3);
+console.log(5>=3);
+console.log(5===3);
+console.log(5==3);
+console.log(true && true);
+console.log(true || false);
+console.log(!true);
+
+let isPos = num > 0 ? "Positive" : "Negative";
+console.log(isPos);
