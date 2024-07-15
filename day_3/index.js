a573cd745e7497c39b290f80de16976b962abc8b
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-15 16:44:42 +0530

    Completed day-3 of 30 days of JS challenge

---
+++ day_3/index.js
@@ -0,0 +1,88 @@
+`use strict`;
+
+// let num = 0;
+// if(num > 0){
+//     console.log(`Positive`);
+// }else if(num < 0){
+//     console.log(`Negative`);
+// }else{
+//     console.log(`Zero`);
+// }
+
+// let age = 19;
+// if(age >= 18){
+//     console.log(`Person Eligible to vote!`);
+// }else{
+//     console.log(`Person not eligible to vote!`);
+// }
+
+// let num1 =256,num2=78,num3 = 153;
+// if(num1 > num2 && num1 > num3){
+//     console.log(`num1 is greater`);
+// }
+// else if(num2 > num1 && num2 > num3){
+//     console.log(`num2 is greater`);
+// }else{
+//     console.log(`num 3 is greater`);
+// }
+
+// const weekDay = 7;
+// switch(weekDay){
+//     case 1:
+//         console.log(`monday`);
+//         break;
+//     case 2:
+//         console.log(`tuesday`);
+//         break;
+//     case 3:
+//         console.log(`wednesday`);
+//         break;
+//     case 4:
+//         console.log(`thursday`);
+//         break;
+//     case 5:
+//         console.log(`friday`);
+//         break;
+//     case 6:
+//        console.log( `saturday`);
+//         break;
+//     case 7:
+//         console.log(`sunday`);;
+//         break;
+// }
+// const marks = 60;
+// switch (true) {
+//   case marks >= 80 && marks <= 100:
+//     console.log(`A`);
+//     break;
+//   case marks >= 70 && marks <= 79:
+//     console.log(`B`);
+//     break;
+//   case marks >= 60 && marks <= 69:
+//     console.log(`C`);
+//     break;
+//   case marks >= 50 && marks <= 59:
+//     console.log(`D`);
+//     break;
+//   case marks >= 0 && marks <= 49:
+//     console.log(`F`);
+//     break;
+//   default:
+//     console.log(`Invalid Entry!`);
+// }
+
+let num = 20;
+const isEven = num%2 === 0 ? 'Is Even' : `Is Odd`;
+// console.log(isEven);
+
+
+// let year = 1600;
+// if(year % 100 === 0 && year % 400 === 0){
+//     console.log(`Leap Year`);
+// }
+// else if(year % 100 !== 0 && year % 4 === 0){
+//     console.log(`Leap Year`);
+// }
+// else{
+//     console.log(`Not a leap year`);
+// }
