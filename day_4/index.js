e4b91f16df9380c44e3bc9897868048908459e0d
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-16 18:49:06 +0530

    completed day-4 of loops

---
+++ day_4/index.js
@@ -0,0 +1,52 @@
+`use strict`;
+
+// for(let i=1;i<=10;i++)
+    // console.log(i);
+// for(let i=1;i<=10;i++)
+    // console.log(`5 X ${i} = ${5*i}`);
+
+
+let total = 0;
+// let i=1;
+// while(i<=10){
+//     total += i;
+//     i++;
+// }
+// console.log(total);
+// let i=10;
+// while(i>=0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+// let fact = 1;
+// let i = 1;
+// do{
+//     fact = fact * i;
+//     i++;
+// }while(i<=5);
+// console.log(fact);
+// for(let i=0;i<5;i++){
+//     let str = ``;
+//     for(let j=0;j<=i;j++){
+//         str += `*`;
+//         console.log(str);
+//     }
+//     console.log(`\n`);
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i === 5)
+//         continue;
+//     console.log(i);
+// }
+//  for(let i=1;i<=10;i++){
+//     if(i === 7)
+//         break;
+//     console.log(i);
+// }
